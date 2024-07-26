436c59b57e18daceeb2919ccaaa4c4945670804a
Ahmed Galal <ahmedgalalmohamed912@gmail.com>
2024-07-26 10:33:32 +0300

    fix: rewirte to use Swagger

--- TaskBloggingPlatform/Program.cs
+++ TaskBloggingPlatform/Program.cs
@@ -55,17 +55,11 @@ namespace TaskBloggingPlatform
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-            else
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+
 
             app.UseHttpsRedirection();
 
